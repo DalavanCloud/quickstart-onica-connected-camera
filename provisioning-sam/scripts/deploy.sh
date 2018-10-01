b7efa373c23f0dfd14e05099bf8c8ffcbe14671e
Ryan <ryan.beck@sturdynetworks.com>
2018-10-01 13:34:23 -0700

    Initial import with stack availability endpoint

---
+++ provisioning-sam/scripts/deploy.sh
@@ -0,0 +1,4 @@
+sam deploy \
+    --template-file packaged.yaml \
+    --stack-name aws-connected-camera-provisioning-stack \
+    --capabilities CAPABILITY_IAM
---
+++ provisioning-sam/scripts/package.sh
@@ -0,0 +1,4 @@
+sam package \
+    --template-file template.yaml \
+    --output-template-file packaged.yaml \
+    --s3-bucket aws-connected-camera-provisioning-sam-template
